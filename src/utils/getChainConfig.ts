1f0d0a30a461616143959da47c9438987e4b4756
Ogulcan Patavi <pataviogulcan@gmail.com>
2023-01-26 17:02:23 +0300

    fix: rpc (#4)

--- src/utils/getChainConfig.ts
+++ src/utils/getChainConfig.ts
@@ -11,14 +11,14 @@ export const getChainConfig = (
       apiKey: process.env.REACT_APP_SPONSOR_API_KEY_MAINNET!,
       chainId: 100,
       target: "0x2dd703a17170C1b03abC26C4D5dc56c9382c5292",
-      rpcUrl: "https://rpc.gnosischain.com",
+      rpcUrl: process.env.REACT_APP_GNOSIS_RPC_URL!,
     };
   } else {
     return {
       apiKey: process.env.REACT_APP_SPONSOR_API_KEY!,
       chainId: 80001,
       target: "0xBf17E7a45908F789707cb3d0EBb892647d798b99",
-      rpcUrl: "https://rpc-mumbai.maticvigil.com",
+      rpcUrl: process.env.REACT_APP_MUMBAI_RPC_URL!,
     };
   }
 };
